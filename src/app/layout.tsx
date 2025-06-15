3c1ce9efbc28d6c52cae30b31d0bce433b2244da
kolarskydominik <kolarskydominik@gmail.com>
2025-06-15 15:49:43 +0200

    added: shadcn components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import './globals.css';
+import { LucideKanban } from 'lucide-react';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import Link from 'next/link';
+import { Button } from '@/components/ui/button';
 import { homePath, ticketsPath } from '@/paths';
 
 const geistSans = Geist({
@@ -33,16 +35,15 @@ export default function RootLayout({
           className="bg-background/60 fixed top-0 right-0 left-0 z-20 flex w-full justify-between
             border-b px-5 py-2.5 backdrop-blur"
         >
-          <div>
-            <Link href={homePath()} className="text-xl font-bold">
-              Home
+          <Button asChild variant={'ghost'}>
+            <Link href={homePath()}>
+              <LucideKanban className="stroke-3" />
+              <h1 className="text-lg font-semibold">TicketBounty</h1>
             </Link>
-          </div>
-          <div>
-            <Link href={ticketsPath()} className="text-lg underline">
-              Tickets
-            </Link>
-          </div>
+          </Button>
+          <Button asChild variant={'outline'}>
+            <Link href={ticketsPath()}>Tickets</Link>
+          </Button>
         </nav>
         <main
           className="bg-secondary/20 flex min-h-svh flex-1 flex-col overflow-x-hidden overflow-y-auto
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ const HomePage = () => {
   return (
     <div className="flex flex-1 flex-col gap-y-8">
       <div>
-        <h2 className="text-3xl font-bold tracking-tight">HomePage</h2>
+        <h2 className="text-3xl font-bold tracking-tight">Home</h2>
         <p className="text-muted-foreground text-sm">
           Your home place to start
         </p>
